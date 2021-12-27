42a17afebcc9da96a8e3351dbef38c47929aabe9
morrisort <31942206+morrisort@users.noreply.github.com>
2021-12-27 16:51:54 -0300

    Update specs.h

--- Seccion2/srcHW/specs.h
+++ Seccion2/srcHW/specs.h
@@ -1,9 +1,10 @@
 #ifndef SPECS_H
 #define SPECS_H
 
-#include <ap_fixed.h>
+//#include <ap_fixed.h>
 //typedef ap_fixed<32,12,AP_RND > T;
-typedef float T;
+typedef int T;
+
 #define M 16
 #define N 16
 #define P 16
